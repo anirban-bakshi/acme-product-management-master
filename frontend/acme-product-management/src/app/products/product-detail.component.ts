14cf8582de3a31df46dac4096c5d386b5d9f60e0
Anirban Bakshi <anirvana.theone@gmail.com>
2020-02-28 00:44:26 +0530

    product-detail component

--- frontend/acme-product-management/src/app/products/product-detail.component.ts
+++ frontend/acme-product-management/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from '../product-list/product';
 
 @Component({
@@ -11,9 +13,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    //+ is a javascript short cut to convert string to number
+    let id = +this.activatedRoute.snapshot.paramMap.get('id'); 
+    this.pageTitle += ` ${id} `;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2019',
+      'description': 'Leaf Rake with 48 inch wooden handle',
+      'price': 19/95,
+      'starRating': 3.2,
+      'imageUrl': "./assets/images/leaf_rake.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
